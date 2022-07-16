3a62e4c1425463f639e00d628e6bd29ef8ca1dfb
Никита Бочаров <bochanikita@yandex.ru>
2022-07-16 13:31:03 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static int DeptCalculate(int dept, int payment)
+        {
+            dept -= payment;
+
+            if (dept <= 0)
+            {
+                Console.WriteLine("Your overpayment is {0}", dept);
+                Console.WriteLine("The dept is repaid");
+                return 1;
+            }
+            else
+                {
+                Console.WriteLine("The amount of dept is {0}", dept);
+                Label1:
+                Console.WriteLine("Enter the amount of payment");
+                payment = Convert.ToInt32(Console.ReadLine());
+
+                if (payment < 100)
+                    {
+                    Console.WriteLine("The payment is less than necessary. The Minimum payment is 100");
+                    goto Label1;
+                    }
+
+                return DeptCalculate(dept, payment);
+                }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int payment;
+            int dept = 700;
+
+            Console.WriteLine("Your dept is {0}", dept);
+            Console.WriteLine("Enter the amount of payment");
+            payment = Convert.ToInt32(Console.ReadLine());
+
+            DeptCalculate(dept, payment);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static int Factorial(int operand)
+        {
+            if (operand == 1)
+                return 1;
+            else
+            { 
+                return operand * Factorial(operand - 1); ;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int operand = 8;
+
+            operand = Factorial(operand);
+            Console.WriteLine(operand);
+
+            Console.ReadLine();
+        }
+    }
+}
